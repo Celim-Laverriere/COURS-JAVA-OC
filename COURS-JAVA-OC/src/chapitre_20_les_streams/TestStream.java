97f2ca0c5632fe7dc3811910c03105c9104ad96c
CELIM <CELIM@DESKTOP-GK647D3>
2018-02-13 16:08:32 +0100

    Chapitre 20 - Manipulez vos données avec les streams => A revoir !

--- COURS-JAVA-OC/src/chapitre_20_les_streams/TestStream.java
+++ COURS-JAVA-OC/src/chapitre_20_les_streams/TestStream.java
@@ -1,7 +1,12 @@
 package chapitre_20_les_streams;
 
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestStream {
@@ -24,6 +29,86 @@ public class TestStream {
 		//Stream.iterate(1, (x) -> x + 1).forEach(System.out::println);
 		
 		Stream.iterate(2d, (x) -> x + 1).limit(100).forEach(System.out::println);
+		
+		Stream<Personne> sp2 = listP.stream();
+		sp2.forEach(System.out::println);
+
+		
+		System.out.println("\nAprès le filtre");
+		sp2 = listP.stream();
+		sp2.filter(x -> x.getPoids() > 50).forEach(System.out::println);
+		
+		
+		System.out.println("\nAprès le filtre et le map");
+		sp = listP.stream();
+		sp. filter(x -> x.getPoids() > 50).map(x -> x.getPoids()).forEach(System.out::println);
+		
+		
+		System.out.println("\nAprès le filtre et le map et reduce");
+		sp = listP.stream();
+		Double sum = sp	.filter(x -> x.getPoids() > 50).map(x -> x.getPoids()).reduce(0.0d, (x,y) -> x+y);
+		System.out.println(sum);
+		
+		System.out.println("\nAprès le filtre et le map et reduce");
+		sp = listP.stream();
+		
+		Optional<Double> sum2 = sp.filter(x -> x.getPoids() > 250).map(x -> x.getPoids()).reduce((x,y) -> x+y);
+
+		if(sum2.isPresent())
+			System.out.println(sum2.get());
+		else
+			System.out.println("Aucun aggrégat de poids...");
+		
+		
+		sp = listP.stream();
+
+		Optional<Double> sum3 = sp	.filter(x -> x.getPoids() > 250)
+									.map(x -> x.getPoids())
+									.reduce((x,y) -> x+y);
+		//Permet de gérer le cas d'erreur en renvoyant 0.0 si isPresent() == false
+		System.out.println(sum3.orElse(0.0));
+		
+		
+		sp = listP.stream();
+
+		long count = sp	.filter(x -> x.getPoids() > 50)
+						.map(x -> x.getPoids())
+						.count();
+
+		System.out.println("Nombre d'éléments : " + count);
+		
+		
+		sp = listP.stream();
+
+		List<Double> ld = sp.filter(x -> x.getPoids() > 50)
+							.map(x -> x.getPoids())
+							.collect(Collectors.toList());
+		System.out.println(ld);
+		
+		
+		String fileName = "F:\\PARCOURS DÉVELOPPEUR D'APPLICATION JAVA\\PROJET 3 - METTEZ VOTRE LOGIQUE A L'EPREUVE\\COURS JAVA OC GIT\\COURS-JAVA-OC\\COURS-JAVA-OC/test.sh";
+		try(Stream<String> sf = Files.lines(Paths.get(fileName))){
+			sf.forEach(System.out::println);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
